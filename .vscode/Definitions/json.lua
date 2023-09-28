4506512fc034e7de8ea6ed747cfb39f9c283cae9
Viktor Kratz <viktorkratz@gmail.com>
2023-09-28 23:46:24 +0200

    Using definition file for language server now

---
+++ .vscode/Definitions/json.lua
@@ -0,0 +1,13 @@
+---@meta
+---@class json
+json={}
+
+---@param jsonStr string
+function json.decode(jsonStr)
+    return {}
+end
+
+---@param tableToEncode table
+function json.encode(tableToEncode)
+    return "{}"
+end
---
+++ .vscode/Definitions/utils.lua
@@ -0,0 +1,11 @@
+---@meta
+
+---@class utils
+utils={}
+
+---@param msg string
+---@param title string|nil
+---@param t table<any,string>
+---@param default any
+function utils.choose(msg,title,t,default)
+end
